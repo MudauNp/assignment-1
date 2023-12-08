4268207f1fc09e6aaa1a9f6c1abc080e6f3ca9e7
MudauNP <5100154@mypillar7uwc.ac.za>
2023-12-08 12:11:58 +0200

    Add project files.

---
+++ assignment 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class BeerSong
+{
+    public void Sing()
+    {
+        for (int bottles = 99; bottles >= 2; bottles--)
+        {
+            Console.WriteLine($"{bottles} bottles of beer on the wall, {bottles} bottles of beer.");
+            Console.WriteLine($"Take one down and pass it around, {bottles - 1} {(bottles - 1 == 1 ? "bottle" : "bottles")} of beer on the wall.");
+                Console.WriteLine();
+        }
+          Console.WriteLine("1 bottle of beer on the wall, 1 bottle of beer");
+    }
+}
+
+class Program
+
+{
+    static void Main()
+    {
+        BeerSong beerSong = new BeerSong();
+        beerSong.Sing();
+    }
+}
+
+
+
+
